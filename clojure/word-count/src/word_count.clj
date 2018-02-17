2e936af851153c48d72daabdc64a9e783abb1077
Wen-Chun Lin <lovecankill@gmail.com>
2018-02-18 00:14:03 +0800

    Solution to word count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,15 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :as str]))
 
-(defn word-count [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn find-valid-words [words-lst]
+  (filter some? (map #(re-find #"[a-zA-Z0-9]+" %) words-lst)))
+
+(defn split-words [words-str]
+  (str/split words-str #" "))
+
+(defn word-count [words-str]
+  (-> words-str
+      str/lower-case
+      split-words
+      find-valid-words
+      frequencies))
